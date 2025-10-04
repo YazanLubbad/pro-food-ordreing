8b9a04b7851bc8aa350744fc5a6c393cf8afea67
YazanLubbad <yazanbassam19@gmail.com>
2025-10-04 14:41:23 +0300

    Initial commit for microservices

---
+++ Desktop/microservice/order-service/src/main/java/com/order_service/order_service/Repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+
+package com.order_service.order_service.Repositories;
+
+import com.order_service.order_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
