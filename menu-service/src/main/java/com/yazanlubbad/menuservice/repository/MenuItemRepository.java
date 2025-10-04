32dc3cbec8b279354dff0c61478165a7fe52b958
YazanLubbad <yazanbassam19@gmail.com>
2025-10-04 14:32:09 +0300

    remove all old files from main branch

--- menu-service/src/main/java/com/yazanlubbad/menuservice/repository/MenuItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yazanlubbad.menuservice.repository;
-
-import com.yazanlubbad.menuservice.model.MenuItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
-}
--- order-service/src/main/java/com/yazanlubbad/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yazanlubbad.orderservice.repository;
-
-import com.yazanlubbad.orderservice.model.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-}
